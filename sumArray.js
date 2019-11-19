b313394232ea49769ea0675be66426a18366d24d
Aldo <aldo.rivadeneira@gmail.com>
2019-11-19 21:46:26 +0100

    add a javascript test file

---
+++ sumArray.js
@@ -0,0 +1,18 @@
+function simpleArraySum(ar) {
+    /*
+     * Write your code here.
+     */
+    var total = 0;
+    var result;
+    
+
+    for(var item in ar){
+        // console.log(item);
+        var index = item;
+        value = ar[index];
+        total = total + value;
+    }
+    console.log(total);
+}
+
+simpleArraySum([1, 2, 3, 4, 10, 11]);
