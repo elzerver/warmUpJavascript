e06283be253fadfb9f1332cee1f302984fa688e4
Aldo <aldo.rivadeneira@gmail.com>
2019-11-23 23:19:26 +0100

    add odd numbers example

---
+++ oddNumbers.js
@@ -0,0 +1,17 @@
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+console.log(ourArray);
+for (var x = 1; x < 10; x += 2){
+    myArray.push(x);
+}
+
+console.log(myArray);
