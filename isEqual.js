b4b660a093529be464ef36994f82925225c31e3f
Aldo <aldo.rivadeneira@gmail.com>
2019-11-29 18:12:56 +0100

    add isEqual example

---
+++ isEqual.js
@@ -0,0 +1,15 @@
+function isEqual(a, b) {
+    // if (a < b) {
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // }
+    
+    // Fix this code
+    return a === b;
+    }
+  
+  // Change these values to test
+  isEqual(10, 15);
+  
