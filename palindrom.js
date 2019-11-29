e2d4aeedda5880b4e24a20adb3b8cd52fb7376da
Aldo <aldo.rivadeneira@gmail.com>
2019-11-29 18:15:12 +0100

    add palinrdrom example

---
+++ palindrom.js
@@ -0,0 +1,20 @@
+/*
+* Stacks
+*  functions push, pop, peek, length 
+*/
+
+var letters = [];
+var word = "racercar";
+var rword; // reverse word
+
+// put letters of word into the stack 
+// pop off the stack in reverse order
+// validation of the word
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
