61cc14b6c760bdf6c1b54f7414ec71693f986628
Aldo <aldo.rivadeneira@gmail.com>
2019-11-29 18:12:41 +0100

    add array example

---
+++ array.js
@@ -0,0 +1,31 @@
+// Example
+var ourArray = [["The universe", 42], 
+["everything", 101010]];
+
+// Only change the code below this line
+// var myArray = [];
+
+// Create a nested array called myArray
+// var myArray = [["first array",1], ["second array",2]];
+
+// console.log(myArray[1]);
+
+// Exercise 2
+
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[0][0];
+console.log(myArray[2][1]);
+
+var shopList = [
+    ["Chocolate bar", 100],
+    ["Muesli", 1000],
+    ["Nuts", 500],
+    ["Vegetables", 100],
+    ["Fruits", 800]
+];
+
+console.log(shopList[0]);
+
